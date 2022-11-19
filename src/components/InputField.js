6b836a320fe6ff6b40987bf0dcb0e84f1e03287e
Rizki fadillah <rizki.fadillah.20@student.is.ittelkom-sby.ac.id>
2022-11-19 13:56:06 +0700

    component inputField -> InputField

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Box, Input} from "native-base";
 
 
-const inputField = ({placeholder, borderRadius, shadow, size, ...restProps}) => {
+const InputField = ({placeholder, borderRadius, shadow, size, ...restProps}) => {
   return (
       <Box borderRadius={borderRadius} shadow={shadow} bg="#FFFFFF">
         <Input borderRadius={borderRadius} borderWidth="0" size={size} {...restProps} />
